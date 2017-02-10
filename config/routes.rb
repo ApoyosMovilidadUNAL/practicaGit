3d4a3af031d751294f3e0a1010f4f5a7e2c53e31
Omar Eraso <oserasoa@unal.edu.co>
2017-02-10 15:04:11 -0500

    route to contact us

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+
 Rails.application.routes.draw do
-  get 'pages/index'
+  root to: 'pages#index'
+  get 'contactUS', to: 'page#contactUS'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'pages#index'
+  
 end
